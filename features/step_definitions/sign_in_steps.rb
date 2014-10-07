2448ab02fad1d7bbd94099e64c83cb5f01d6e683
Daniel Sun <dan.mi.sun@gmail.com>
2014-10-07 13:06:40 +0100

    @wip admin login feature

--- features/step_definitions/sign_in_steps.rb
+++ features/step_definitions/sign_in_steps.rb
@@ -1,17 +1,25 @@
 Given(/^that an admin exists$/) do
-  pending # express the regexp above with the code you wish you had
+  @email = Faker::Internet.email
+
+  @admin = User.create!(:username => Faker::Name.first_name,
+                       :email => @email,
+                       :password => 'password',
+                       :type => 'Admin'
+                       )
 end
 
 Given(/^that an admin is on the login page$/) do
-  pending # express the regexp above with the code you wish you had
+  visit new_user_session_path
 end
 
 When(/^an admin fills in their username$/) do
-  pending # express the regexp above with the code you wish you had
+  fill_in("Email", :with => @email)
 end
 
 When(/^they fill in their password$/) do
-  pending # express the regexp above with the code you wish you had
+  fill_in("Password", :with => 'password')
+  save_and_open_page
+  click_button("Sign in")
 end
 
 Then(/^they should be logged in\.$/) do
