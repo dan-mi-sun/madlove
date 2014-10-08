1175921c411e0157392f4726fa7acbaeae84596f
Daniel Sun <dan.mi.sun@gmail.com>
2014-10-08 15:34:36 +0100

    passing integration tests for admin login feature

--- features/step_definitions/sign_in_steps.rb
+++ features/step_definitions/sign_in_steps.rb
@@ -18,11 +18,10 @@ end
 
 When(/^they fill in their password$/) do
   fill_in("Password", :with => 'password')
-  save_and_open_page
   click_button("Sign in")
 end
 
 Then(/^they should be logged in\.$/) do
-  pending # express the regexp above with the code you wish you had
+  page.has_content?('Signed in successfully.')
 end
 
