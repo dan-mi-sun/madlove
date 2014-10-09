6ec550227206d2cf68e27443bed19e53c071876f
Daniel Sun <dan.mi.sun@gmail.com>
2014-10-09 12:15:29 +0100

    @wip add routes and blank view templates for static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,22 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!
+
+  def what
+  end
+
+  def get_involved
+  end
+
+  def who
+  end
+
+  def mailing_list
+  end
+
+  def submit
+  end
+
+  def audio
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,14 @@ Rails.application.routes.draw do
   
   root to: "pages#home"
 
+  get "pages/get_involved", path: '/get-involved'
+  
+  get "pages/who", path: '/who'
+
+  get "pages/mailing_list", path: '/mailing-list'
+
+  get "pages/submit", path: '/submit'
+
+  get "pages/audio", path: '/audio'
+
 end
