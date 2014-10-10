988b948810d84b3fc34da646ba075c573a3f723f
Daniel Sun <dan.mi.sun@gmail.com>
2014-10-10 15:26:42 +0100

    refactor static page test to check for relevant keywords

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,49 +7,49 @@ describe PagesController do
 
     describe "Home page" do
 
-      it "should have the content 'Sample App'" do
+      it "should have the content 'Home'" do
         visit '/'
-        expect(page).to have_content('Sample App')
+        expect(page).to have_content('Home')
       end
     end
 
     describe "Get involved page" do
 
-      it "should have the content 'Help'" do
+      it "should have the content 'Involved'" do
         visit '/get-involved'
-        expect(page).to have_content('Help')
+        expect(page).to have_content('Involved')
       end
     end
 
     describe "Who's involved page" do
 
-      it "should have the content 'Help'" do
+      it "should have the content 'Who'" do
         visit '/who'
-        expect(page).to have_content('Help')
+        expect(page).to have_content('Who')
       end
     end
 
     describe "Mailing-list page" do
 
-      it "should have the content 'Help'" do
+      it "should have the content 'Mailing'" do
         visit '/mailing-list'
-        expect(page).to have_content('Help')
+        expect(page).to have_content('Mailing')
       end
     end
 
     describe "Submit idea page" do
 
-      it "should have the content 'Help'" do
+      it "should have the content 'Idea'" do
         visit '/submit'
-        expect(page).to have_content('Help')
+        expect(page).to have_content('Idea')
       end
     end
 
     describe "Hear audio site page" do
 
-      it "should have the content 'Help'" do
+      it "should have the content 'Audio'" do
         visit '/audio'
-        expect(page).to have_content('Help')
+        expect(page).to have_content('Audio')
       end
     end
   end
